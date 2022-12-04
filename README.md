ca56d8dcbd80d863d144dde9c3d7600f3e3db48c
Jainam <theoneeyetriangle@gmail.com>
2022-12-05 01:37:42 +0530

    Complete Code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Crawl-Website
+# Crawl Starbuck
+Hi this is my first python project.I got this idea from reddit and so here we are it took me 2 days to coplete this project and I am proud of this as my first project
+
+The idea is simple make a csv file of address of all the starbucks location near Delhi. I have used selenium but if I am famous enough tht asomeone is reading this please do suggest other libraies.
+
+This python script unfortunately only runs in startbucks india website, as html tags will be diffrent for diffrent countries wesbste if you are using this code changing only url will not work
+
+To change the location froom Delhi to some other place see line 15 of the script
+
+I couldnt clean up the results list in lines 40-44 using smart usage of selenium so again if you have idea future me or good stranger please do share 
+
+This is a terrible README i hope i can do better next time.
---
+++ crawl_Starbucks.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+import csv
+
+#Loading the page 
+starbucksUrl = 'https://www.starbucks.com/store-locator?map=39.635307,-101.337891,5z'
+driver = webdriver.Chrome() 
+driver.get(starbucksUrl)
+sleep(5)
+
+#Automating search box fill up
+inputFormula = driver.find_element(By.NAME,'place')
+inputFormula.send_keys('Delhi India')
+inputFormula.send_keys(Keys.ENTER)
+sleep(2)
+
+#Getting the number of nearby cafes
+resultList = driver.find_elements(By.CSS_SELECTOR, 'article.base___3LiS9.linkOverlay.sb-global-gutters.py3.relative')
+resultSize = len(resultList)
+iterations = int(resultSize)
+results=[]
+
+#Getting th address of each cafe
+for i in range(iterations):
+    #Clicking the infobtn
+    infoBtn = resultList[i].find_element(By.CSS_SELECTOR, 'a.relative.pl5.color-textBlackSoft')
+    driver.execute_script("arguments[0].click();", infoBtn)
+    sleep(1)
+    #Extracting the address
+    getStoreAddress = driver.find_elements(By.CSS_SELECTOR, 'div.gridItem.size6of12') 
+    results.append(getStoreAddress[0].get_attribute('innerHTML'))
+    #Clicking the close button
+    element = driver.find_element(By.CSS_SELECTOR, 'button.sb-iconButton.relative.sb-closeButton.sb-overlay__closeBtn.color-textBlackSoft')
+    element.click()
+    sleep(1)
+    
+#Removing unneccesary html syntax, I couldnt extract the address without them using selenium    
+rep = [('<span class="block">',''),('<p>',''),('</span>',' '),('</p>',''),('amp; ','')] 
+driver.close()
+for k in range(len(results)):
+    for i,j in rep:
+        results[k] = results[k].replace(i,j)
+
+#making the csv file
+with open('Restaurants.csv', mode='a', newline='') as outputFile:
+    restaurantCSV = csv.writer(outputFile, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+    
+    restaurantName = 'Starbucks'
+    country = 'India'
+    for restaurant in results:
+        street = restaurant.split("/n")[0]
+        zipCode = restaurant.split(",")[-1]
+
+        restaurantCSV.writerow([restaurantName, street, zipCode, country])
+
